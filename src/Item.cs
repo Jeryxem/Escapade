f85fb9fbfbeec2ca47c707b2418712249aad6173
FlareLine <101133651@swin.edu.au>
2017-05-20 19:30:30 +1000

    Added Item class. Revamping some of the iheritance structures so they are more related the actual direction of the game.

---
+++ src/Item.cs
@@ -0,0 +1,43 @@
+﻿namespace Escapade.item
+{
+	public abstract class Item
+	{
+		int _id;
+		int _meta;
+		string _name;
+
+		#region Properties
+		public int Id {
+			get {
+				return _id;
+			}
+			set {
+				_id = value;
+			}
+		}
+		public int Meta {
+			get {
+				return _meta;
+			}
+			set {
+				_meta = value;
+			}
+		}
+		public string Name {
+			get {
+				return _name;
+			}
+			set {
+				_name = value;
+			}
+		}
+		#endregion Properties
+
+		public Item (int id, int meta, string name)
+		{
+			Id = id;
+			Meta = meta;
+			Name = name;
+		}
+	}
+}
